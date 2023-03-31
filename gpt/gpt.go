26676ccfb42f55ec38a7ef6f5052fb566f802ad6
kpcmcn <kpcmcn@163.com>
2023-03-31 12:48:14 +0800

    修改超时时间

--- gpt/gpt.go
+++ gpt/gpt.go
@@ -131,7 +131,7 @@ func httpRequestCompletions(msg string, runtimes int) (*ChatGPTResponseBody, err
 
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "Bearer "+cfg.ApiKey)
-	client := &http.Client{Timeout: 15 * time.Second}
+	client := &http.Client{Timeout: 30 * time.Second}
 	response, err := client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("client.Do error: %v", err)
